f5b222997da727cbe3eadf78814332be9726af33
suichanwa <augustinnegura746@gmail.com>
2023-12-14 15:22:14 -0800

    init app

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []); 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Timer: {timer} seconds</Text>
       <StatusBar style="auto" />
     </View>
   );
